a987e0aaab5f0a1304dbaf51e77bf801c2640f89
LukMona <monikaa.lukowska@gmail.com>
2016-06-08 12:06:08 +0200

    dragula file update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,24 +1,15 @@
 /**
-* Setting HTML 
+* @description Setting HTML 
 */
 var game = new Game();
 
 var container = document.getElementById("gameSection");
 game.render(container);
 
-//making elements draggable
-// function allowDrop(ev){
-//   ev.preventDefault();
-// }
+/** 
+* @description Using Drag'n'Drop Dragula
+*/
 
-// function drag(ev){
-//   ev.dataTransfer.setData("text", ev.target.id)
-// }
-
-// function drop(ev){
-//   ev.preventDefault();
-//   var data = ev.dataTransfer.getData("text");
-//   ev.target.appendChild(document.innerHTML(data));
-// }
-
-//getting id from drop space
+// var droppingLetters = document.querySelectorAll(".drop");
+// console.log(droppingLetters);
+// dragula([document.querySelector(".dragging-letters"), document.querySelector(".drop-letters")]);
