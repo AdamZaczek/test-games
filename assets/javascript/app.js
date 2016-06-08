42dd96aa7141f115d31820bc05bb7681a339252e
LukMona <monikaa.lukowska@gmail.com>
2016-06-08 09:17:38 +0200

    dnd v1

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,21 @@
+//setting HTML inside index
 var game = new Game();
 
 var container = document.getElementById("gameSection");
 game.render(container);
+
+//making elements draggable
+
+function allowDrop(ev){
+	ev.preventDefault();
+}
+
+function drag(ev){
+	ev.dataTransfer.setData("text", ev.target.id)
+}
+
+function drop(ev){
+	ev.preventDefault();
+	var data = ev.dataTransfer.getData("text");
+	ev.target.appendChild(document.getElementById(data));
+}
--- assets/javascript/games/game.js
+++ assets/javascript/games/game.js
@@ -15,6 +15,14 @@
       "assets/images/letter-o.png"
     ];
 
+    this.lettersId = [
+      "3-c",
+      "7-g",
+      "23-w",
+      "11-k",
+      "15-o"
+    ];
+
     /* classes */ 
     this.usingClasses = [
       "main-image",
@@ -41,9 +49,9 @@
     //dropping section
     this.dropSpace = [
       "<div class=" + this.usingClasses[2] + ">",
-        "<div class=" + this.usingClasses[3] + "></div>",
-        "<div class=" + this.usingClasses[3] + "></div>",
-        "<div class=" + this.usingClasses[3] + "></div>",
+        "<div class=" + this.usingClasses[3] + " ondrop='drop(event)' ondragover='allowDrop(event)'></div>",
+        "<div class=" + this.usingClasses[3] + " ondrop='drop(event)' ondragover='allowDrop(event)'></div>",
+        "<div class=" + this.usingClasses[3] + " ondrop='drop(event)' ondragover='allowDrop(event)'></div>",
       "</div>"
     ].join('');
 
@@ -52,15 +60,30 @@
       "<div class=" + this.usingClasses[1] + "></div>"
     ].join('');
 
-    //map of mixed letters
-    this.lettersMap = this.letters.map(function(url){
-      return "<div class='drag'><img src=" + url + "></div>";
-    });
+    // //map of mixed letters
+    // this.lettersMap = this.letters.map(function(url){
+    //   return "<div class='drag' draggable='true' ondragstart='drag(event)'><img src=" + url + "></div>";
+    // });
 
     //dragging section
     this.dragSpace = [
       "<div class=" + this.usingClasses[4] + ">",
-        this.lettersMap.join(''),
+        // this.lettersMap.join(''),
+        "<div class='drag' draggable='true' ondragstart='drag(event)'>",
+          "<img src="+ this.letters[0] + " id=" + this.lettersId[0] + ">",
+        "</div>",
+        "<div class='drag' draggable='true' ondragstart='drag(event)'>",
+          "<img src="+ this.letters[1] + " id=" + this.lettersId[1] + ">",
+        "</div>",
+        "<div class='drag' draggable='true' ondragstart='drag(event)'>",
+          "<img src="+ this.letters[2] + " id=" + this.lettersId[2] + ">",
+        "</div>",
+        "<div class='drag' draggable='true' ondragstart='drag(event)'>",
+          "<img src="+ this.letters[3] + " id=" + this.lettersId[3] + ">",
+        "</div>",
+        "<div class='drag' draggable='true' ondragstart='drag(event)'>",
+          "<img src="+ this.letters[4] + " id=" + this.lettersId[4] + ">",
+        "</div>",
       "</div>"
     ].join('');
 
