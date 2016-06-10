76ac27470f23e40bd0bc0d9c68ccc3840a22cc96
LukMona <monikaa.lukowska@gmail.com>
2016-06-10 17:28:28 +0200

    Updated context and beggining of the success/failure events

--- assets/javascript/games/letter-arrangment.js
+++ assets/javascript/games/letter-arrangment.js
@@ -8,30 +8,53 @@ define([], function() {
    */
   LetterArrangement.prototype.render = function(container) {
     var context = {
-          imgUrl: "assets/images/cow.jpg",
-          letters: [
-            {
-             letter: "C",
-            },
-            {
-              letter: "K",
-            },
-            {
-              letter: "O",
-            },
-            {
-              letter: "W"
-            },
-            {
-              letter: "G"
-            }
-          ]
-      };
+      riddles: {
+            imgUrl: "assets/images/cow.jpg",
+            letters: [
+              {
+                letter: "C"
+              },
+              {
+                letter: "K"
+              },
+              {
+                letter: "O"
+              },
+              {
+                letter: "W"
+              },
+              {
+                letter: "G"
+              }
+            ],
+            correctAnswer: "COW"
+      }
+    };
 
     var source = document.getElementById("entry").innerHTML;
     var template = Handlebars.compile(source);
     var html = template(context);
+
     container.innerHTML = html;
+
+    function result(){
+      var allDrop = document.querySelectorAll(".drop");
+      var allIsField = [];
+
+      for (var i = 0; i < allDrop.length; i++){
+        var dropCheck = allDrop[i].hasChildNodes();
+        allIsField.push(dropCheck);
+        }
+
+      function checkDroppingSpace(drop){
+        return drop === true;
+      }
+
+      if(allIsField.every(checkDroppingSpace) === true){
+      }
+    }
+
+    result();
   };
 
   var container = document.getElementById("gameSection");
