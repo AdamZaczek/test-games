a98cb43a7fc2fe68aa3fcd25395aceb642536f27
LukMona <monikaa.lukowska@gmail.com>
2016-06-14 09:43:17 +0200

    Updated app.js with correct requirejs to the LetterArrangment

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,4 +2,13 @@ requirejs.config({
   baseUrl: "assets/javascript",
 });
 
-requirejs(["games/letter-arrangment"]);
+requirejs([
+  "games/letter-arrangment"
+], function(
+    LetterArrangement
+    ) {
+    var container = document.getElementById("gameSection");
+    var letterArrangement = new LetterArrangement();
+
+    letterArrangement.render(container);
+  });
