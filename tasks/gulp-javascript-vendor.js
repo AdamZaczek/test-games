e304046fb80a6ac4e6631c711e5cd5bcd146d866
LukMona <monikaa.lukowska@gmail.com>
2016-06-15 14:40:09 +0200

    Gulp-javascript-vendor file modified: deleted line with RequireJS text plugin

--- tasks/gulp-javascript-vendor.js
+++ tasks/gulp-javascript-vendor.js
@@ -7,8 +7,7 @@ module.exports = function javascriptVendor() {
   return gulp.src([
       "node_modules/handlebars/dist/handlebars.min.js",
       "node_modules/dragula/dist/dragula.min.js",
-      "node_modules/requirejs/require.js",
-      "node_modules/text/text.js"
+      "node_modules/requirejs/require.js"
     ])
     .pipe(plugins.concat("vendors.js"))
     .pipe(gulp.dest("assets/javascript"));
