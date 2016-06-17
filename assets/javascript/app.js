3b01df5e92fd10f520e48eb70bfd5c908253c670
LukMona <monikaa.lukowska@gmail.com>
2016-06-17 11:05:30 +0200

    Added to requirejs reference to the game-result.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,19 @@
 requirejs.config({
   baseUrl: "assets/javascript",
   paths: {
-    text: "text"
+    text: "text",
+    gameResult: "utilities/game-result"
   }
 });
 
 requirejs([
   "games/letter-arrangment"
 ], function(
-  letterArrangement
+  letterArrangement, gameResult
 ) {
   var container = document.getElementById("gameSection");
-
   letterArrangement.render(container);
+
+  var mainContainer = document.getElementById("game");
+  letterArrangement.gameResult.mainElement = mainContainer;
 });
